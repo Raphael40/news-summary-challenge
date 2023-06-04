e16d87b645340ec8c0136ed8299f48553be37deb
Julian Festing <julfesting@gmail.com>
2023-06-04 17:48:15 +0100

    pass test for no query fetch search

--- newsClient.test.js
+++ newsClient.test.js
@@ -7,12 +7,34 @@ describe ('Client class', () => {
     fetch.resetMocks();
   });
 
-  it("calls fetch and loads data", async () => {
+  it("calls fetch and loads data by newest", async () => {
     const client = new NewsClient();
   
     const mockResponse = {
       response: {
         orderBy: "newest",
+        results: [{
+          sectionId: 'random'
+        }]
+      }
+    };    
+  
+    fetch.mockResponse(JSON.stringify(mockResponse));
+  
+    const returnedDataFromApi = await client.fetchNewsData();
+    expect(returnedDataFromApi.response).toHaveProperty('orderBy', 'newest');
+    expect(returnedDataFromApi.response.results[0]).toHaveProperty('sectionId', 'random');
+  });
+
+  it("calls fetch and loads data for politics by newest", async () => {
+    const client = new NewsClient();
+  
+    const mockResponse = {
+      response: {
+        orderBy: "newest",
+        results: [{
+          sectionId: 'politics'
+        }]
       }
     };    
   
@@ -20,5 +42,6 @@ describe ('Client class', () => {
   
     const returnedDataFromApi = await client.fetchNewsData('politics');
     expect(returnedDataFromApi.response).toHaveProperty('orderBy', 'newest');
+    expect(returnedDataFromApi.response.results[0]).toHaveProperty('sectionId', 'politics');
   });
 })
