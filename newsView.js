2b8c8dbdff208d901ae9ba6212a6ac6a5a1ee910
Julian Festing <julfesting@gmail.com>
2023-08-11 12:47:41 +0100

    made the title of each article a link to the original article

--- newsView.js
+++ newsView.js
@@ -12,6 +12,7 @@ class NewsView {
     });
   }
 
+  // Filters what is displayed on the page
   displayNews(query) {
     const existingNews = document.querySelectorAll('.article')
     existingNews.forEach((article) => {article.remove()})
@@ -19,16 +20,20 @@ class NewsView {
     const createArticleElement = (article) => {
       const div = document.createElement('div');
       div.classList.add('article');
-  
+      
+      const link = document.createElement('a')
+      link.href = article.webUrl
+      div.appendChild(link)
+      
       const h2 = document.createElement('h2');
       h2.textContent = article.webTitle;
       h2.classList.add('webTitle');
-      div.appendChild(h2);
+      link.appendChild(h2);
   
       const img = document.createElement('img');
       img.src = article.fields.thumbnail;
       div.appendChild(img);
-  
+
       const p = document.createElement('p');
       p.textContent = article.fields.bodyText;
       div.appendChild(p);
@@ -36,6 +41,7 @@ class NewsView {
       return div;
     };
   
+  // Updates page content when submit button is clicked with a value
   const updateNewsDisplay = (model) => {
     const newsData = model.getNewsData();
     const mainContainerElement = document.getElementById('main-container');
